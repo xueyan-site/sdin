a3eb905b58f529c226356e05117d1887e2fa2d1a
xueyan-site <yang@xueyan.site>
2021-01-23 18:52:07 +0800

    fix: support display download information when create project

--- src/scripts/create-react-application/index.ts
+++ src/scripts/create-react-application/index.ts
@@ -1,7 +1,7 @@
 import ora from 'ora'
-import { execSync } from 'child_process'
 import ReactApplicationProject from '../../projects/react-application-project'
 import { copyProjectTemplate } from '../../utils/write'
+import { executeSync } from '../../utils/exec'
 
 /**
  * 主程序
@@ -15,12 +15,12 @@ export default async function main(project: ReactApplicationProject) {
    * 下载node_modules
    */
   const downloadOra = ora('downloading node modules').start()
-  execSync(`cd ${project.path} && yarn`)
+  executeSync(`cd ${project.path} && yarn`)
   downloadOra.succeed(`downloaded node modules successfully`)
   /**
    * 初始化git
    */
   const initGitOra = ora('initializing git repository').start()
-  execSync(`cd ${project.path} && git init && git add . && git commit -m "chore: project created"`)
+  executeSync(`cd ${project.path} && git init && git add . && git commit -m "chore: project created"`)
   initGitOra.succeed(`initialized git repository successfully`)
 }
--- src/scripts/create-react-package/index.ts
+++ src/scripts/create-react-package/index.ts
@@ -1,8 +1,8 @@
 import ora from 'ora'
-import { execSync } from 'child_process'
 import ReactPackageProject from '../../projects/react-package-project'
 import { copyProjectTemplate } from '../../utils/write'
 import { withPath } from '../../utils/path'
+import { executeSync } from '../../utils/exec'
 
 /**
  * 主程序
@@ -16,18 +16,18 @@ export default async function main(project: ReactPackageProject) {
    * 下载node_modules
    */
   const downloadOra = ora('downloading node modules').start()
-  execSync(`cd ${project.path} && yarn`)
+  executeSync(`cd ${project.path} && yarn`)
   downloadOra.succeed(`downloaded node modules successfully`)
   /**
    * 下载 doc node_modules
    */
   const downloadDocOra = ora('downloading doc node modules').start()
-  execSync(`cd ${withPath(project.path, 'doc')} && yarn`)
+  executeSync(`cd ${withPath(project.path, 'doc')} && yarn`)
   downloadDocOra.succeed(`downloaded doc node modules successfully`)
   /**
    * 初始化git
    */
   const initGitOra = ora('initializing git repository').start()
-  execSync(`cd ${project.path} && git init && git add . && git commit -m "chore: project created"`)
+  executeSync(`cd ${project.path} && git init && git add . && git commit -m "chore: project created"`)
   initGitOra.succeed(`initialized git repository successfully`)
 }
